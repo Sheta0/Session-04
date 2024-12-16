fa367968fe77b9025b024fd7f33f57823ab22aa7
Sheta0 <theimpossible000@gmail.com>
2024-12-16 11:35:04 +0200

    Adding Files

---
+++ Assignment 04/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Assignment_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+
+            //int[] Arr01 = { 1, 2, 9, 15, 54, 5, 3 };
+            //int[] Arr02 = { 10, 8, 20, 4, 6, 7, 31 };
+
+            //int[] MergedArray = new int[Arr01.Length * 2];
+
+            //Array.Copy(Arr01, 0, MergedArray, 0, Arr01.Length);
+            //Array.Copy(Arr02, 0, MergedArray, Arr01.Length, Arr02.Length);
+
+            //Array.Sort(MergedArray);
+
+            //foreach (int i in MergedArray)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+            #endregion
+
+            #region Q2
+
+            //int[] Arr = { 1, 2, 1, 3, 2, 2 };
+
+            //Array.Sort(Arr);
+
+            //int Count = 1;
+            //for (int i = 0; i < Arr.Length; i++)
+            //{
+            //    if (i < Arr.Length - 1 && Arr[i] == Arr[i + 1])
+            //    {
+            //        Count++;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Number {Arr[i]} : {Count}");
+            //        Count = 1;
+            //    }
+            //}
+            #endregion
+
+            #region Q3
+
+            //int[] Numbers = { 21, 12, 5, 8, 91 };
+            //int Max = Numbers[0];
+            //int Min = Numbers[0];
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    if (Numbers[i] > Max)
+            //        Max = Numbers[i];
+
+            //    if (Numbers[i] < Min)
+            //        Min = Numbers[i];
+
+            //}
+            //Console.WriteLine(Max);
+            //Console.WriteLine(Min);
+
+            #endregion
+
+
+
+        }
+    }
+}
